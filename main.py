5de1d8b9ffbefa6b2fb5935c121ad033bb7982fb
Navid <navidjalili3@gmail.com>
2023-08-02 19:28:23 +0330

    Add error handling

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-# Improved code efficiency
+# Added error handling
 import socket
-
-# Create a UDP socket
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as client_socket:
-    client_socket.settimeout(1)  # Set socket timeout to 1 second
-
-    # Send ping message to server
-    client_socket.sendto(b'Ping', ('localhost', 12345))
-
-    try:
-        # Receive pong message from server
-        pong_message, server_address = client_socket.recvfrom(1024)
-    except socket.timeout:
-        print("Packet lost: No reply from server")
+try:
+    # Create a UDP socket
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as client_socket:
+        client_socket.settimeout(1)  # Set socket timeout to 1 second
+        # Send ping message to server
+        client_socket.sendto(b'Ping', ('localhost', 12345))
+        try:
+            # Receive pong message from server
+            pong_message, server_address = client_socket.recvfrom(1024)
+        except socket.timeout:
+            print("Packet lost: No reply from server")
+except socket.error as e:
+    print(f"Socket error occurred: {e}")
