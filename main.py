a30609edb2f5a7ba67ad7cf2ab0e8e890c2d9de4
Navid <navidjalili3@gmail.com>
2023-08-02 19:24:29 +0330

    Implemente pong message receiving

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+# Implemented pong message receiving
 import socket
-
 # Create a UDP socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
 # Send ping message to server
-client_socket.sendto(b'Ping', ('localhost', 12000))
+client_socket.sendto(b'Ping', ('localhost', 12345))
+# Receive pong message from server
+pong_message, server_address = client_socket.recvfrom(1024)
